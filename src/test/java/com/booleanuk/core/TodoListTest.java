e0678b8a0f6845b11e859e5f10b92001705d0a50
gremble0 <hermanstornes@gmail.com>
2024-08-15 09:35:35 +0200

    Update model, add test for add method

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testTodoListAdd() {
+        TodoList todoList = new TodoList();
+        todoList.add(new Task("Water plants"));
+        Assertions.assertEquals(todoList.size(), 1);
+
+        // Adding same task should only overwrite existing task
+        todoList.add(new Task("Water plants"));
+        Assertions.assertEquals(todoList.size(), 1);
+
+        todoList.add(new Task("Grocery shopping"));
+        Assertions.assertEquals(todoList.size(), 2);
     }
 }
