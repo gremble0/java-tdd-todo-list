1408fdd921fcfcea0b8690c5bcb6a03429e9cc1c
gremble0 <hermanstornes@gmail.com>
2024-08-15 11:19:47 +0200

    Implement setDescription

--- src/main/java/com/booleanuk/extension/TodoListExtended.java
+++ src/main/java/com/booleanuk/extension/TodoListExtended.java
@@ -13,4 +13,15 @@ public class TodoListExtended extends TodoList {
     // Will only be reached if task with taskDescription does not exist
     throw new NonExistentTaskException(ID);
   }
+
+  public void setTaskDescription(int ID, String description) throws NonExistentTaskException {
+    for (Task task : this.tasks)
+      if (task.ID == ID) {
+        task.description = description;
+        return;
+      }
+
+    // Will only be reached if task with taskDescription does not exist
+    throw new NonExistentTaskException(ID);
+  }
 }
