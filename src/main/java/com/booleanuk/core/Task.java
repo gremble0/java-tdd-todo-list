cf2cf96b8a7ced5bc5806cf39f2bd267f014b142
gremble0 <hermanstornes@gmail.com>
2024-08-15 09:42:49 +0200

    Pass add test

---
+++ src/main/java/com/booleanuk/core/Task.java
@@ -0,0 +1,31 @@
+package com.booleanuk.core;
+
+public class Task {
+  private static int taskCounter = 0;
+
+  private int ID;
+  private String description;
+  private boolean completed;
+
+  public Task(String description, boolean completed) {
+    this.ID = taskCounter++;
+    this.description = description;
+    this.completed = completed;
+  }
+
+  public Task(String description) {
+    this(description, false);
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Task))
+      return false;
+
+    Task otherTask = (Task) other;
+    if (otherTask.description.equals(this.description))
+      return true;
+    else
+      return false;
+  }
+}
--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,17 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TodoList {
+  private List<Task> tasks = new ArrayList<>();
+
+  public int size() {
+    return this.tasks.size();
+  }
+
+  public void add(Task task) {
+    if (!this.tasks.contains(task))
+      this.tasks.add(task);
+  }
 }
